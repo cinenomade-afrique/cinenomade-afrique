aeb3fd3931c4404d2d92ecd7f102f025d7cb1760
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-06-25 17:30:20 +0200

    chore: install sass

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "index.html", "./*.js"
+  content: [ "index.html", "./*.ejs","./*.js"
   ],
   theme: {
     extend: {},
