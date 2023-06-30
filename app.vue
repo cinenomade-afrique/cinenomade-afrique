53e5c585064e481da9d207a79623997c31cb8336
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-06-30 17:12:30 +0200

    feat: add menu basic

--- app.vue
+++ app.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
-      <NuxtLayout>
-          <NuxtPage />
-      </NuxtLayout>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/nos-actions">Nos actions</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/festival">Le Festival</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/teams">L'équipe</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/media">Médias</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/contact">Contact</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-    <div>Index Page</div>
+    <div>
+        Home page
+    </div>
 </template>
