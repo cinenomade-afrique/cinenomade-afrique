bbaf41f00a9d6869b1c944d4b3732d426adfb36c
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-07-01 22:41:09 +0200

    feat: add card example components

--- pages/teams.vue
+++ pages/teams.vue
@@ -2,8 +2,23 @@
     useHead({
         title: "Cine Nomade Afrique - L'équipe",
     });
+
+    const cardData = ref([
+        { id: 1, image: 'vite.svg', title: 'My journey with Vue', text: 'test test test' },
+        { id: 2, image: 'vite.svg', title: 'Blogging with Vue', text: 'test test test'},
+        { id: 3, image: 'vite.svg', title: 'Why Vue is so fun', text: 'test test test' }
+    ])
 </script>
 
 <template>
-    <div>Notre Équipe Page</div>
+    <div>
+        <h1>Notre Équipe Page</h1>
+        <Card
+            v-for="card in cardData"
+            :key="card.id"
+            :title="card.title"
+            :text="card.text"
+            :image="card.image"
+        />
+    </div>
 </template>
