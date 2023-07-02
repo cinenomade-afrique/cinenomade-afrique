dddba87a9203ce8c8da1a24e32705e0cbc6db5a0
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-07-02 18:02:48 +0200

    feat: refacto component footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,11 @@
 <script setup></script>
 
 <template>
-    <div>
-        <footer>Pied de page</footer>
-    </div>
+    <footer class="bg-slate-800 text-white">
+        <div class="container mx-auto px-4">
+            <p class="text-5xl">
+                Pied de page
+            </p>
+        </div>
+    </footer>
 </template>
--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,30 +1,52 @@
 <script setup></script>
 
 <template>
-    <div>
-        <header>
-            <nav>
-                <ul>
+    <header class="mt-6 header-sheet-1">
+        <div class="flex justify-between container mx-auto px-4">
+            <NuxtLink to="/">
+                <img class="header-logo-cna" src="@/assets/images/logo/Logo_CNA_couleur.png" alt="CineNomadeAfrique">
+            </NuxtLink>
+            <nav class="flex flex-col justify-center header-navbar">
+                <ul class="inline-block">
                     <li>
-                        <NuxtLink to="/">Home</NuxtLink>
+                        <NuxtLink class="py-2.5 px-5" to="/">Accueil</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/nos-actions">Nos actions</NuxtLink>
+                        <NuxtLink class="py-2.5 px-5" to="/nos-actions">Nos actions</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/festival">Le Festival</NuxtLink>
+                        <NuxtLink class="py-2.5 px-5" to="/festival">Le Festival</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/teams">L'équipe</NuxtLink>
+                        <NuxtLink class="py-2.5 px-5" to="/teams">L'équipe</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/media">Médias</NuxtLink>
+                        <NuxtLink class="py-2.5 px-5" to="/media">Médias</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/contact">Contact</NuxtLink>
+                        <NuxtLink class="py-2.5 px-5" to="/nos-partenaires">Nos partenaires</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink class="py-2.5 px-5" to="/contact">Contact</NuxtLink>
                     </li>
                 </ul>
             </nav>
-        </header>
-    </div>
+        </div>
+    </header>
 </template>
+
+<style lang="scss">
+    .header-sheet-1 {
+        min-height: 80px;
+    }
+
+    .header-logo-cna {
+        width: 60px;
+    }
+
+    .header-navbar {
+        ul li {
+            display: inline-block;
+        }
+    }
+</style>
