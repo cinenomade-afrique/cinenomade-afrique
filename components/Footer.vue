93f36e6eb12e5e4f838f62e4cd207a31dec53cf2
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-07-15 20:24:49 +0200

    feat: add auto year update to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,10 +1,22 @@
-<script setup></script>
+<script>
+    export default {
+        mounted() {
+            const yearAutoUpdate = () => {
+                let footerCopyrightYear = new Date();
+                if (document.querySelector(".var--footer-copyright") !== null) {
+                    document.querySelector(".var--footer-copyright").innerText = footerCopyrightYear.getFullYear();
+                }
+            }
+            yearAutoUpdate();
+        }
+    }
+</script>
 
 <template>
     <footer class="bg-slate-800 text-white var--footer">
         <div class="container max-w-screen-xl mx-auto px-5 text-center">
-            <p class="text-2xl">
-                Copyright 2023 © Ciné Nomade Afrique - Tous droits réservés
+            <p>
+                Copyright <span class="var--footer-copyright"></span> &copy; Ciné Nomade Afrique - Tous droits réservés
             </p>
         </div>
     </footer>
