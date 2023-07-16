34a7fe721c3abe61ea3c25adc67b96b8daea3f85
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-07-16 16:47:26 +0200

    feat: set footer container

--- components/Footer.vue
+++ components/Footer.vue
@@ -13,10 +13,13 @@
 </script>
 
 <template>
-    <footer class="bg-slate-800 text-white var--footer">
-        <div class="container max-w-screen-xl mx-auto px-5 text-center">
-            <p>
-                Copyright <span class="var--footer-copyright"></span> &copy; Ciné Nomade Afrique - Tous droits réservés
+    <footer class="bg-cyan-700 text-white var--footer">
+        <div class="flex flex-col divide-y container max-w-screen-xl mx-auto px-5">
+            <p class="text-center py-14">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis aut vel dolore. Aliquid numquam reprehenderit, unde autem, nam temporibus eligendi totam labore rem eum dolore veritatis, repellat odio aliquam eius.
+            </p>
+            <p class="text-xs text-center py-5">
+                Copyright <span class="var--footer-copyright"></span> &copy; Ciné'Nomade Afrique - Tous droits réservés
             </p>
         </div>
     </footer>
