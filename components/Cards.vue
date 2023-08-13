8a8c6ec01a1450e06ff8ef5fe845b805ed46f6ad
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-08-13 21:04:33 +0200

    feat: add component cards to teams page

--- components/Cards.vue
+++ components/Cards.vue
@@ -21,7 +21,7 @@
 
 <template>
     <div
-        class="block max-w-[22rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        class="block max-w-[22rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 mb-4">
         <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
             <img class="rounded-t-lg" v-bind:src="'/assets/images/phototheque/' + image" alt="" />
             <a href="#!">
@@ -38,7 +38,7 @@
                 {{ text }}
             </p>
             <button type="button"
-                class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                class="inline-block rounded bg-red-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-red-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-red-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                 data-te-ripple-init data-te-ripple-color="light">
                 Button
             </button>
--- pages/teams.vue
+++ pages/teams.vue
@@ -4,31 +4,45 @@
     });
 
     const cardData = ref([{
-            id: 1,
-            image: 'vite.svg',
-            title: 'My journey with Vue',
-            text: 'test test test'
-        },
-        {
-            id: 2,
-            image: 'vite.svg',
-            title: 'Blogging with Vue',
-            text: 'test test test'
-        },
-        {
-            id: 3,
-            image: 'vite.svg',
-            title: 'Why Vue is so fun',
-            text: 'test test test'
-        }
-    ])
+      id: 1,
+      image: 'partenaires-cna-pictures-1.jpg',
+      title: 'Aïssa DIABY',
+      text: 'Présidente de l’Association & Coordinatrice générale'
+    },
+    {
+      id: 2,
+      image: 'partenaires-cna-pictures-2.jpg',
+      title: 'Aboubacar-Naby CAMARA',
+      text: 'Responsable Admnistratif'
+    },
+    {
+      id: 3,
+      image: 'partenaires-cna-pictures-1.jpg',
+      title: 'Sidney Cadot',
+      text: 'Responsable de la programation'
+    },
+    {
+      id: 4,
+      image: 'partenaires-cna-pictures-2.jpg',
+      title: 'Anné Cissé',
+      text: 'Résponsable dévelopement'
+    },
+    {
+      id: 5,
+      image: 'partenaires-cna-pictures-1.jpg',
+      title: 'Assita GASSAMA (Tasta)',
+      text: 'Coordinatrice logistique et événementielle'
+    }
+  ])
 </script>
 
 <template>
     <section>
         <div class="container max-w-screen-xl mx-auto my-14 px-5 md:px-10">
-            <h1 class="text-5xl mt-14">Notre Équipe Page</h1>
-            <Card v-for="card in cardData" :key="card.id" :title="card.title" :text="card.text" :image="card.image" />
+            <h1 class="text-5xl mt-14">Notre Équipe</h1>
+            <div class="grid grid-cols-3 gap-3 my-14">
+                <Cards v-for="card in cardData" :key="card.id" :title="card.title" :text="card.text" :image="card.image" />
+            </div>
         </div>
     </section>
 </template>
