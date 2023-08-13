17661b0a4a80eecec0b7777c52706e8bb0dc3bc4
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-08-13 19:25:34 +0200

    feat: add new cards style component

---
+++ components/Cards.vue
@@ -0,0 +1,51 @@
+<script>
+    export default {
+        name: "Cards",
+        mounted: async function () {
+            const {
+                Ripple,
+                initTE
+            } = await import("tw-elements");
+
+            initTE({
+                Ripple
+            });
+        },
+        props: {
+            image: String,
+            title: String,
+            text: String,
+        }
+    };
+</script>
+
+<template>
+    <div
+        class="block max-w-[22rem] rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init data-te-ripple-color="light">
+            <img class="rounded-t-lg" v-bind:src="'/assets/images/phototheque/' + image" alt="" />
+            <a href="#!">
+                <div
+                    class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                </div>
+            </a>
+        </div>
+        <div class="p-6">
+            <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                {{ title }}
+            </h5>
+            <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                {{ text }}
+            </p>
+            <button type="button"
+                class="inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                data-te-ripple-init data-te-ripple-color="light">
+                Button
+            </button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/nos-actions.vue
+++ pages/nos-actions.vue
@@ -1,26 +1,30 @@
-<script lang="ts">
+<script setup lang="ts">
   useHead({
     title: "Cine Nomade Afrique - Nos actions",
   });
-  // export default {
-  //   name: "Collapse",
-  //   mounted: async function () {
-  //     const {
-  //       Collapse,
-  //       initTE
-  //     } = await import("tw-elements");
 
-  //     initTE({
-  //       Collapse
-  //     });
-  //   },
-  // };
+  const cardData = ref([{
+      id: 1,
+      image: 'partenaires-cna-pictures-1.jpg',
+      title: 'Action 2022',
+      text: 'test test test'
+    },
+    {
+      id: 2,
+      image: 'partenaires-cna-pictures-2.jpg',
+      title: 'Action 2023',
+      text: 'test test test'
+    }
+  ])
 </script>
 
 <template>
   <section>
     <div class="container max-w-screen-xl mx-auto px-5 md:px-10">
       <h1 class="text-5xl mt-14">Nos actions Page</h1>
+      <div class="flex my-14">
+        <Cards v-for="card in cardData" :key="card.id" :title="card.title" :text="card.text" :image="card.image" />
+      </div>
     </div>
   </section>
 </template>
