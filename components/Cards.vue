299b3baa22b28828b20a2be424d443f4c635396d
Slimane GASSAMA <slimane.gassama@gmail.com>
2023-08-24 20:18:24 +0200

    feat: add modal to cards component

--- components/Cards.vue
+++ components/Cards.vue
@@ -3,18 +3,22 @@
         name: "Cards",
         mounted: async function () {
             const {
+                Modal,
                 Ripple,
                 initTE
             } = await import("tw-elements");
 
             initTE({
+                Modal,
                 Ripple
             });
         },
         props: {
             image: String,
             title: String,
-            text: String,
+            subTitle: String,
+            idLabel: String,
+            text: String
         }
     };
 </script>
@@ -35,14 +39,81 @@
                 {{ title }}
             </h5>
             <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                {{ text }}
+                {{ subTitle }}
             </p>
-            <button type="button"
+            <button
+                type="button"
                 class="inline-block rounded bg-red-600 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-red-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-red-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-red-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                data-te-ripple-init data-te-ripple-color="light">
-                Button
+                data-te-ripple-init
+                data-te-toggle="modal"
+                :data-te-target="`#test-${idLabel}`"
+                data-te-ripple-color="light">
+                Click
             </button>
         </div>
+    </div>
+
+    <!-- Modal -->
+    <div
+      data-te-modal-init
+      class="fixed left-0 top-40 z-[1055] hidden h-full w-full overflow-y-auto overflow-x-hidden outline-none"
+      :id="`test-${idLabel}`"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div
+        data-te-modal-dialog-ref
+        class="pointer-events-none relative w-auto mx-4 translate-y-[-50px] opacity-0 transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:max-w-[500px]">
+        <div
+          class="min-[576px]:shadow-[0_0.5rem_1rem_rgba(#000, 0.15)] pointer-events-auto relative flex w-full flex-col rounded-md border-none bg-white bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-600">
+          <div
+            class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+            <!--Modal title-->
+            <h5
+              class="text-xl font-medium leading-normal text-neutral-800 dark:text-neutral-200"
+              id="exampleModalLabel">
+              {{ title }}
+            </h5>
+            <!--Close button-->
+            <button
+              type="button"
+              class="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+              data-te-modal-dismiss
+              aria-label="Close">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="h-6 w-6">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+
+          <!--Modal body-->
+          <div class="relative overflow-y-auto p-4" data-te-modal-body-ref>
+            {{ text }}
+          </div>
+
+          <!--Modal footer-->
+          <div
+            class="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+            <button
+              type="button"
+              class="inline-block rounded bg-red-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-red-700 transition duration-150 ease-in-out hover:bg-red-accent-100 focus:bg-red-accent-100 focus:outline-none focus:ring-0 active:bg-red-accent-200"
+              data-te-modal-dismiss
+              data-te-ripple-init
+              data-te-ripple-color="light">
+              Fermer
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
--- pages/teams.vue
+++ pages/teams.vue
@@ -7,31 +7,41 @@
       id: 1,
       image: 'partenaires-cna-pictures-1.jpg',
       title: 'Aïssa DIABY',
-      text: 'Présidente de l’Association & Coordinatrice générale'
+      subTitle: 'Présidente de l’Association & Coordinatrice générale',
+      idLabel: 'example1',
+      text: "Aïssa Diaby est une réalisatrice et scénariste Franco-guinéenne de 29 ans, diplômée d’un Master d’études Internationales et Européennes à Sciences Po Grenoble. Aïssa a occupé plusieurs postes au sein des institutions internationales et de la fonction d'État en France. Elle a notamment travaillé en République de Guinée en tant que Vice commissaire en charge du programme de l’UNESCO “Conakry World Book Capital 2017”, puis elle a été nommée Responsable de la salle de cinéma CanalOlympia Kaloum avant de retourner en France et occuper la fonction de correspondante communication et chargée de mission au Ministère de l’intérieur à Paris. Parallèlement à sa carrière professionnelle Aïssa s’est formée au métier de la création audiovisuelle et cinématographique."
     },
     {
       id: 2,
       image: 'partenaires-cna-pictures-2.jpg',
       title: 'Aboubacar-Naby CAMARA',
-      text: 'Responsable Admnistratif'
+      subTitle: 'Responsable Admnistratif',
+      idLabel: 'example2',
+      text: "Aboubacar-Naby CAMARA est un artiste plasticien, réalisateur, médiateur culturel en collectivité et acteur associatif culturel franco-guinéen. Diplômé de l'École des Arts de l'Université Paris 1 Panthéon Sorbonne, il a obtenu une Licence et un Master en Art et Images du Vivant. En tant qu'artiste, Aboubacar-Naby explore divers médiums, tels que la photographie, la vidéo, l'installation et la sculpture. Il a co-réalisé le documentaire 'Welcome to Conakry' avec Aïcha Diaby pour promouvoir la capitale guinéenne. Son premier court-métrage 'Cum Laude' avec Bamar Kane, produit par Tribudom est sorti en 2023"
     },
     {
       id: 3,
       image: 'partenaires-cna-pictures-1.jpg',
       title: 'Sidney Cadot',
-      text: 'Responsable de la programation'
+      subTitle: 'Responsable de la programation',
+      idLabel: 'example3',
+      text: "Sidney CADOT-SAMBOSI est comédienne et critique de cinéma. Elle investit le champ de la création cinématographique et théâtral à travers ses rencontres (Dieudonné Niangouna, Jean-Yves Ruf, Jean-Pierre Bekolo, Nelson Makengo entre autres). De ses études de philosophie à Paris, elle retiendra cette expression de Gilles Deleuze qui préconise 'd'avoir toujours un petit morceau d'une nouvelle terre'."
     },
     {
       id: 4,
       image: 'partenaires-cna-pictures-2.jpg',
       title: 'Anné Cissé',
-      text: 'Résponsable dévelopement'
+      subTitle: 'Résponsable dévelopement',
+      idLabel: 'example4',
+      text: "Diplômée du Celsa (une école française de communication et de journalisme qui fait partie de la Sorbonne), Anne a travaillé plusieurs années chez Google Arts & Culture en produisant des expériences numériques pour l'écosystème culturel (y compris le MET, le J. Paul Getty Museum, Carnegie Hall...) tout en affinant ses outils en tant que scénariste et réalisatrice. De 2016 à 2020, elle a écrit et réalisé 5 courts métrages pendant son temps libre. En 2018, elle a quitté Google pendant un an pour aider à créer et écrire VAMPIRES, une série originale Netflix. Elle a également rejoint LUPIN en tant que scénariste pour la saison 1."
     },
     {
       id: 5,
       image: 'partenaires-cna-pictures-1.jpg',
       title: 'Assita GASSAMA (Tasta)',
-      text: 'Coordinatrice logistique et événementielle'
+      subTitle: 'Coordinatrice logistique et événementielle',
+      idLabel: 'example5',
+      text: "Tasta Gassama est une professionnelle accomplie dans le domaine du business et de l'événementiel. Franco-guinéenne, elle a su mettre à profit sa double culture pour développer une vision unique et une approche innovante dans son travail. Son professionnalisme et son dévouement sont largement reconnus dans le secteur."
     }
   ])
 </script>
@@ -40,8 +50,15 @@
     <section>
         <div class="container max-w-screen-xl mx-auto my-14 px-5 md:px-10">
             <h1 class="text-5xl mt-14">Notre Équipe</h1>
-            <div class="grid grid-cols-3 gap-3 my-14">
-                <Cards v-for="card in cardData" :key="card.id" :title="card.title" :text="card.text" :image="card.image" />
+            <div class="grid md:grid-cols-2 md:gap-3 lg:grid-cols-3 lg:gap-4 justify-center my-14">
+                <Cards v-for="card in cardData"
+                  :key="card.id"
+                  :idLabel="card.idLabel"
+                  v-bind:id="card.id"
+                  :title="card.title"
+                  :subTitle="card.subTitle"
+                  :image="card.image"
+                  :text="card.text" />
             </div>
         </div>
     </section>
